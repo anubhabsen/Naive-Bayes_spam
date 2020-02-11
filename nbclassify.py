edc498c0ac7ac8d62c0b09395913c3ca53d24b1a
Anubhab Sen <anubhabsen@gmail.com>
2020-02-10 23:43:58 -0800

    perform naive bayes computations

---
+++ nbclassify.py
@@ -0,0 +1,53 @@
+import json
+import sys
+import os
+import glob
+import math
+
+correct_predicts = 0
+output_string = ""
+with open('nbmodel.txt', 'r') as fp:
+	data = fp.read()
+
+data = json.loads(data)
+spam_p = data[0]
+ham_p = data[1]
+master_dict = data[2]
+
+pys = []
+for file in glob.iglob('**/*', recursive=True):
+    if file.endswith('.txt'):
+        pys.append(file)
+
+pys = pys[2:]
+
+for filename in pys:
+	prob_s = math.log(spam_p)
+	prob_h = math.log(ham_p)
+	with open(filename, "r", encoding="latin1") as f:
+	    content = f.readlines()
+	content = [x.strip() for x in content]
+	for line in content:
+		x = line.split()
+		for j in x:
+			j = j.lower()
+			if j not in master_dict:
+				continue
+			prob_s += math.log(master_dict[j][0])
+			prob_h += math.log(master_dict[j][1])
+	if prob_s > prob_h:
+		output_string += 'spam ' + filename + '\n'
+		# print('spam ' + filename)
+		if filename.endswith('.spam.txt'):
+			correct_predicts += 1
+	else:
+		output_string += 'ham ' + filename + '\n'
+		# print('ham ' + filename)
+		if filename.endswith('.ham.txt'):
+			correct_predicts += 1
+
+tot_files = len(pys)
+print(correct_predicts / tot_files * 100)
+f = open("nboutput.txt", "w")
+f.write(output_string)
+f.close()
