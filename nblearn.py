fce0daa95cd8d0c3de2c6eba79f71ae70ef3bd28
Anubhab Sen <anubhabsen@gmail.com>
2020-02-10 22:57:28 -0800

    simpler structure and smoothing probabilities

--- nblearn.py
+++ nblearn.py
@@ -3,32 +3,33 @@ import os
 import sys
 from operator import add
 
-spam = {}
-ham = {}
-allmails = {}
 spam_words = 0
 ham_words = 0
-tot_words = 0
+spams = 0
+hams = 0
 master_dict = {}
 
-def readtodict(filename, dictionary, clas):
+def readtodict(filename, clas):
+	if clas == 'spam':
+		global spams
+		spams += 1
+	else:
+		global hams
+		hams += 1
 	with open(filename, "r", encoding="latin1") as f:
 	    content = f.readlines()
 	content = [x.strip() for x in content]
 	for line in content:
 		x = line.split()
 		for j in x:
-			master_dict[j.lower()] = master_dict.get(j.lower(), [0, 0, 0]) +
-			dictionary[j.lower()] = dictionary.get(j.lower(), 0) + 1
-			global tot_words
-			tot_words += 1
 			if clas == 'spam':
+				master_dict[j.lower()] = list(map(add, master_dict.get(j.lower(), [0, 0, 0]), [1, 0 ,1]))
 				global spam_words
 				spam_words += 1
 			elif clas == 'ham':
+				master_dict[j.lower()] = list(map(add, master_dict.get(j.lower(), [0, 0, 0]), [0, 1 ,1]))
 				global ham_words
 				ham_words += 1
-			allmails[j.lower()] = allmails.get(j.lower(), 0) + 1
 
 # root_dir = sys.argv[1]
 root_dir = "Spam or Ham"
@@ -39,23 +40,18 @@ for i in os.listdir(root_dir):
 		if j == 'ham':
 			for k in os.listdir(root_dir + i +'/' + j + '/'):
 				filename = root_dir + i +'/' + j + '/' + k
-				readtodict(filename, ham, 'ham')
+				readtodict(filename, 'ham')
 		elif j == 'spam':
 			for k in os.listdir(root_dir + i +'/' + j + '/'):
 				filename = root_dir + i +'/' + j + '/' + k
-				readtodict(filename, spam, 'spam')
+				readtodict(filename, 'spam')
+vocab_size = len(master_dict)
+for i in master_dict:
+	master_dict[i][0] = (master_dict[i][0] + 1) / (spam_words + vocab_size)
+	master_dict[i][1] = (master_dict[i][1] + 1) / (ham_words + vocab_size)
 
-# for i in allmails:
-# 	if spam.get(i, 0) + ham.get(i, 0) != allmails[i]:
-# 		print(i)
-
-for i in spam:
-	spam[i] = spam[i] / spam_words
-
-for i in ham:
-	ham[i] = ham[i] / ham_words
-
-all_data = {'spam': spam, 'ham': ham, 'allmails': allmails, 'spam_words': spam_words, 'ham_words': ham_words, 'tot_words': tot_words}
+spam_p = spams / (spams + hams)
+ham_p = hams / (spams + hams)
 
 with open('nbmodel.txt', 'w') as fp:
-    json.dump(all_data, fp)
+    json.dump([spam_p, ham_p, master_dict], fp)
